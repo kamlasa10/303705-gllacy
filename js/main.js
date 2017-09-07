a4f09f64c7a2e50bfa7274941d07242924d7fd30
kamlasa10 <kamlasa@ukr.net>
2017-09-07 15:48:39 +0300

    Подключил интерактивную карту с формой

--- js/main.js
+++ js/main.js
@@ -2,15 +2,49 @@
 var btn = document.querySelector('.btn-recall');
 var popup = document.querySelector('.modal-feedback');
 var close = document.querySelector('.modal-content-close');
+var user = popup.querySelector('[name=name]');
+var email = popup.querySelector('[name=email]')
+var form = popup.querySelector('.feedback-form');
+var overlay = document.querySelector('.overlay');
+var storage = localStorage.getItem('user');
 
 btn.addEventListener('click', function (evt) {
   evt.preventDefault();
   popup.classList.add('modal-show');
+  overlay.classList.add('overlay-show');
+  if (storage) {
+    user.value = storage;
+    email.focus();
+  }
+  else {
+    user.focus();
+  }
 
 });
 
 close.addEventListener('click', function (evt) {
   evt.preventDefault();
   popup.classList.remove('modal-show');
+  overlay.classList.remove('overlay-show');
+});
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains('modal-show')) {
+      popup.classList.remove('modal-show');
+      overlay.classList.remove('overlay-show');
+    }
+  }
+});
+
+form.addEventListener('submit', function (evt) {
+ if (!user.value || !email.value ) {
+   evt.preventDefault();
+   console.log('Введите Имя и адрес электроной почты')
+ } else {
+   localStorage.setItem('user', user.value);
+ }
+
 
 });
+
